20c699490afe534922f0253245dff8c5ee723c6f
H2A7M <138702009+H2A7M@users.noreply.github.com>
2023-07-05 23:57:05 +0700

    Exercise 4

---
+++ Exercise4_Akkharin_S.py
@@ -0,0 +1,9 @@
+Foundation_English = 80
+General_Business = 75
+Computer_Systems = 70
+Computer_Programming = 85
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Computer_Systems)
+print("Computer Programming :",Computer_Programming)
